7102efa994df5764e5ce3aa50d26667245d287ba
Suellen Leite <suellenpll@gmail.com>
2026-04-24 02:48:18 -0300

    Update bronze_customers.py

--- airflow/dags/bronze_customers.py
+++ airflow/dags/bronze_customers.py
@@ -24,7 +24,10 @@ import logging
 # CONFIGURAÇÕES
 # =============================================================================
 
-AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/home/felipe/desafio_prevision/airflow")
+AIRFLOW_HOME = os.environ.get(
+    "AIRFLOW_HOME",
+    os.path.join(os.path.expanduser("~"), "desafio_prevision", "airflow")
+)
 CSV_PATH     = os.path.join(AIRFLOW_HOME, "data", "bronze", "customers.csv")
 DB_PATH      = os.path.join(AIRFLOW_HOME, "data", "prevision.db")
 TABLE_NAME   = "bronze_customers"
